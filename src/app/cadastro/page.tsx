a673dccd46638fef3da650d46ed2ca192dd3a3bc
riandte <rianduarte200@gmail.com>
2025-07-16 00:51:34 -0300

    Configuração do projeto e criação da tela de login

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,10 @@
+import { Tela } from '../../components/main';
+
+export default function cadastro() {
+	return (
+		<div className="">
+			<h1>Cadastro</h1>
+			<Tela />
+		</div>
+	);
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { Tela } from '../../components/main';
+
+export default function cadastro() {
+	return (
+		<div className="">
+			<h1>LOGIN</h1>
+			<Tela />
+		</div>
+	);
+}
